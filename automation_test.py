cf30348ad1dd15e478d1177a660a221fd5374a47
Martin Lazarov <m.lazarov89@gmail.com>
2025-01-25 12:37:01 +0200

    Initial commit for MoneyGaming QA Test

---
+++ automation_test.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+def test_registration_form():
+    # Web driver
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+
+    try:
+        # Navigating to the website
+        driver.get("https://moneygaming.qa.gameaccount.com/")
+
+        # Click the JOIN NOW button to open the registration page
+        join_now_button = driver.find_element(By.LINK_TEXT, "JOIN NOW!")
+        join_now_button.click()
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "form"))
+        )
+
+        title_dropdown = Select(driver.find_element(By.ID, "title"))
+        title_dropdown.select_by_visible_text("Mr")  # I have to replace "Mr" with desired value if needed (check again)
+
+        # first and surname
+        first_name_field = driver.find_element(By.ID, "forename")
+        first_name_field.send_keys("John")
+
+        surname_field = driver.find_element(By.NAME, "map(lastName)")
+        surname_field.send_keys("Doe")
+
+        terms_checkbox = driver.find_element(By.NAME, "map(terms)")
+        terms_checkbox.click()
+
+        # Submit the form by clicking the JOIN NOW button
+        join_now_button = driver.find_element(By.ID, "form")
+        join_now_button.submit()
+
+        # Have to validate the error message under the date of birth box
+        error_message = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//label[@for='dob']//following-sibling::div[@class='error']"))
+        )
+
+        assert error_message.text == "This field is required", "Error message does not match!"
+
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    test_registration_form()
