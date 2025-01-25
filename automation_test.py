8dfb2c19f774d5c22dc92b529e089218f0d84782
Martin Lazarov <m.lazarov89@gmail.com>
2025-01-25 19:53:11 +0200

    Remove user-data-dir option and add additional Chrome options in Selenium

--- automation_test.py
+++ automation_test.py
@@ -3,13 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 def test_registration_form():
-    # Set Chrome options to handle potential conflicts
+    # Set Chrome options
     options = webdriver.ChromeOptions()
-    options.add_argument("--no-sandbox")  # Handle sandbox issues
-    options.add_argument("--disable-dev-shm-usage")  # Handle shared memory issues
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
 
     # Web driver
     driver = webdriver.Chrome(options=options)
