bace62be51b6f035321c99dabd22496ce7345329
Martin Lazarov <m.lazarov89@gmail.com>
2025-01-27 09:14:32 +0200

    Final test before submit version 2

--- automation_test.py
+++ automation_test.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import time
-from datetime import datetime   # Added datetime just to confirm DOB
+from datetime import datetime  # Added datetime to calculate DOB
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -16,10 +16,10 @@ def test_registration_form():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-extensions")
-    # options.add_argument("--headless")  # Use headless mode for CI stability
+    options.add_argument("--headless")  # Use headless mode for CI stability, remove it for local testing
 
     # Create a unique user data directory for each session
-    unique_dir = f"/tmp/chrome_user_data_{os.getpid()}"
+    unique_dir = f"/tmp/chrome_user_data_{os.getpid()}_{int(time.time())}"
     options.add_argument(f"--user-data-dir={unique_dir}")
     options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.6834.110 Safari/537.36"
@@ -28,7 +28,6 @@ def test_registration_form():
     # WebDriver setup
     driver = webdriver.Chrome(options=options)
     driver.set_page_load_timeout(180)  # Increase page load timeout
-    driver.maximize_window()
 
     try:
         # Retry logic for loading the page
@@ -85,7 +84,7 @@ def test_registration_form():
         year_dropdown = Select(driver.find_element(By.ID, "dobYear"))
         year_dropdown.select_by_visible_text(dob_year)
 
-        # Click on Agree to terms and conditions
+        # Agree to terms and conditions
         terms_checkbox = driver.find_element(By.NAME, "map(terms)")
         terms_checkbox.click()
 
@@ -94,8 +93,7 @@ def test_registration_form():
 
         # Validate that no error message appears for the date of birth
         dob_error = driver.find_elements(
-            By.XPATH,
-            "//label[@for='dob']" # //following-sibling::div[@class='error']"
+            By.XPATH, "//label[@for='dob']//following-sibling::div[@class='error']"
         )
 
         assert len(dob_error) == 0, "Unexpected error message for date of birth!"
